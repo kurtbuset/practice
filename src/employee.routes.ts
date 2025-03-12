92997db460a69071d7ab40ac05c0566577ad7940
WHUASHAI <jamesbaloro99@gmail.com>
2025-03-12 19:21:13 +0800

    case 5

--- src/employee.routes.ts
+++ src/employee.routes.ts
@@ -4,6 +4,10 @@ import { Department } from "./entity/Department";
 import Joi from "joi";
 
 import express, { Request, Response } from "express";
+<<<<<<< refs/remotes/origin/gonzales
+=======
+import { LessThan, Like } from "typeorm";
+>>>>>>> local
 const employeeRouter = express.Router();
 
 
@@ -54,10 +58,102 @@ employeeRouter.get("/employees/search", async (req: Request, res: Response) => {
       return res.status(404).json({ msg: `No employees found with name containing: ${searchTerm}` });
     }
 
+<<<<<<< refs/remotes/origin/gonzales
     return res.status(200).json({ msg: "Employees found", employees });
   } catch (err) {
     console.error("Error searching employees:", err);
     return res.status(500).json({ msg: "Internal server error" });
+=======
+    // Update salary to 90000
+    EmployeeToUpdate.salary = 90000;
+
+    await EmployeeRepository.save(EmployeeToUpdate);
+
+    return res
+    .status(200)
+    .json({ message: `Employee ${EmployeeID} salary updated successfully to 90000` });
+  }
+  catch(error){
+    console.error("Error updating employee:", error);
+    return res.status(500).json({ error: "Internal server error" });
+  }
+})
+
+// case 4
+employeeRouter.delete('/api/employees/:id', async (req: Request, res: Response) => {
+  try{
+    const { id } = req.params
+    const employeeRepo = AppDataSource.getRepository(Employee)
+
+    const employee = await employeeRepo.findOneBy({
+      id: Number(id)
+    })  
+
+    if(!employee) return res.status(404).json({ message: "Employee not found" });
+
+    employee.isActive = false
+
+    await employeeRepo.save(employee)
+    
+    return res.status(201).json({ msg: `employee ${employee} set to false`, employee});
+  }
+  catch(error){
+    console.error("Error updating employee:", error);
+    return res.status(500).json({ error: "Internal server error" });
+  }
+})
+
+// Case 5: 
+employeeRouter.get("/api/employees/search", async (req: Request, res: Response) => {
+  try {
+    const { name } = req.query;
+
+    
+    if (!name || typeof name !== "string") {
+      return res.status(400).json({ error: "Search term (name) is required and must be a string." });
+    }
+
+    const employeeRepo = AppDataSource.getRepository(Employee);
+
+   
+    const employees = await employeeRepo.find({
+      where: { name: Like(`%${name}%`) },
+      relations: ["department"],
+    });
+
+    if (employees.length === 0) {
+      return res.status(404).json({ message: "No employees found matching the search term." });
+    }
+
+    return res.status(200).json({ employees });
+  } catch (error) {
+    console.error("Error searching employees:", error);
+    return res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+// case 7
+employeeRouter.get('/api/employees/:id/tenure', async (req: Request, res: Response) => {
+  try{
+    const employeeRepo = AppDataSource.getRepository(Employee)
+    const employee = await employeeRepo.findOneBy({
+      id: Number(req.params.id)
+    })
+
+    if(!employee) return res.status(404).json({ message: "Employee not found" });
+
+    const hireDate = new Date(employee.hireDate);
+    const currentDate = new Date();
+    const yearsOfService = currentDate.getFullYear() - hireDate.getFullYear();
+
+    // const hoursOfService = Math.floor((currentDate.getTime() - hireDate.getTime()) / (1000 * 60 * 60));
+
+    return res.json({employeeId: employee.id, name: employee.name, yearsOfService: yearsOfService})
+  }
+  catch(error){
+    console.error("Error updating employee:", error);
+    return res.status(500).json({ error: "Internal server error" });
+>>>>>>> local
   }
 });
 employeeRouter.post("/api/employees", async (req: Request, res: Response) => {
